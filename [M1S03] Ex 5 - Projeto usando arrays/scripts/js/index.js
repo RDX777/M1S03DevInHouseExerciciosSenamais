2292f6991445148e4be3af340ccf438942ebdadf
Anderson Tome <anderson_tome@outlook.com.br>
2022-07-17 14:38:10 -0300

    Exercicio 5

---
+++ [M1S03] Ex 5 - Projeto usando arrays/scripts/js/index.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+
+    var camarote = [
+        {
+            nome : "José",
+            sobrenome : "Almeida",
+            setor : "Camarote",
+            idade : 50,
+        },
+        {
+            nome : "Paulo",
+            sobrenome : "Barros",
+            setor : "Camarote",
+            idade : 19,
+        },
+    ]
+    var pista = [
+        {
+            nome : "Carla",
+            sobrenome : "Alves",
+            setor : "Pista",
+            idade : 32,
+        },
+        {
+            nome : "Batista",
+            sobrenome : "Antonio",
+            setor : "Pista",
+            idade : 20,
+        },
+    ]
+    var arquibancada = [
+        {
+            nome : "João",
+            sobrenome : "Nunes",
+            setor : "Arquibancada",
+            idade : 42,
+        },
+        {
+            nome : "Roberta",
+            sobrenome : "Maria",
+            setor : "Arquibancada",
+            idade : 38
+        },
+
+    ]
+
+    var listaDeConvidados = arquibancada
+
+    console.log("Variavel iniciada com arquibancada:", arquibancada)
+
+}
